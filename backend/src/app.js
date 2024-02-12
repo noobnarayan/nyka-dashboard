7de6949e7f3feaf963b6b8a2069ff6ceeadc64ff
Narayan Das <workmail.narayan@gmail.com>
2024-02-12 19:43:53 +0530

    add: app and index code

---
+++ backend/src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+export const app = express();
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "your-production-url"
+        : "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+import productRouter from "./routes/product.routes.js";
+
+app.use("/api", userRouter);
+app.use("/api", productRouter);
